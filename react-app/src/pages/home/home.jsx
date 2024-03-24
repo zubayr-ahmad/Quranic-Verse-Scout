d69f560b951b02411459548d498b1d578789e113
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-03-24 14:35:03 +0500

    styling of main img and search bar

--- react-app/src/pages/home/home.jsx
+++ react-app/src/pages/home/home.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import './home.css'
 import Search_bar from '/src/components/search_bar/search_bar'
 import Verses_box from '/src/components/verses_box/verses_box'
+import Home_Image from '/src/components/home_image/home_image'
 function Home() {
   return (
     <div className="container">
-      <img src="src/assets/images/img03.avif" alt="Quran Image" />
+      <Home_Image />
       <Search_bar />
       <Verses_box />
     </div>
